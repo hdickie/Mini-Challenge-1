6b7e2621ff59079848ff71c264030720d1063c82
hdickie <hdickie@dons.usfca.edu>
2017-05-29 21:32:41 -0700

    freaky car!

--- recordsToPaths.py
+++ recordsToPaths.py
@@ -8,8 +8,56 @@ Created on Mon May 29 11:20:35 2017
 import csv
 import os
 import re
+from datetime import datetime as dt
 
 os.chdir("C:/Users/Hume Dickie/Desktop/Github/Mini-Challenge-1/data/")
+
+writeVisits = True
+writePaths = True
+writedfEntries = True
+
+mapAbbrev = {"entrance0" : "E0",
+             "entrance1" : "E1",
+             "entrance2" : "E2",
+             "entrance3" : "E3",
+             "entrance4" : "E4",
+             "camping0" : "C0",
+             "camping1" : "C1",
+             "camping2" : "C2",
+             "camping3" : "C3",
+             "camping4" : "C4",
+             "camping5" : "C5",
+             "camping6" : "C6",
+             "camping7" : "C7",
+             "camping8" : "C8",
+             "general-gate0" : "GG0",
+             "general-gate1" : "GG1",
+             "general-gate2" : "GG2",
+             "general-gate3" : "GG3",
+             "general-gate4" : "GG4",
+             "general-gate5" : "GG5",
+             "general-gate6" : "GG6",
+             "general-gate7" : "GG7",
+             "general-gate8" : "GG8",
+             "ranger-stop0" : "R0",
+             "ranger-stop1" : "R1",
+             "ranger-stop2" : "R2",
+             "ranger-stop3" : "R3",
+             "ranger-stop4" : "R4",
+             "ranger-stop5" : "R5",
+             "ranger-stop6" : "R6",
+             "ranger-stop7" : "R7",
+             "gate0" : "G0",
+             "gate1" : "G1",
+             "gate2" : "G2",
+             "gate3" : "G3",
+             "gate4" : "G4",
+             "gate5" : "G5",
+             "gate6" : "G6",
+             "gate7" : "G7",
+             "gate8" : "G8",
+             "ranger-base" : "B"
+             }
 
 class VisitRecord:    
     def __init__(self,time,gate,visitorId,neverEntered):
@@ -23,14 +71,59 @@ class VisitRecord:
         self.path.append(gate)
         
     def toString(self):
-        assert len(self.path) == len(self.times)
+        #assert len(self.path) == len(self.times)
         returnValue = self.visitorId + "; "
         for i in range(0,len(self.path)):
             if (i == (len(self.path) - 1)):
-                returnValue += self.times[i] + ": " + self.path[i] + "\n"
+                returnValue += str(self.times[i]) + ": " + self.path[i] + "\n"
             else:
-                returnValue += self.times[i] + ": " + self.path[i] + ", "
+                returnValue += str(self.times[i]) + ": " + self.path[i] + ", "
         return returnValue
+        
+    def pathString(self):
+        pathString = self.visitorId + "; "
+        pathString += str(self.times[0]) + "; "
+        pathString += self.path[0] +" "        
+        
+        #compute time deltas
+        timeDeltas = []
+        for i in range(1,len(self.times)):
+            delta = str(self.times[i] - self.times[i-1])
+            timeDeltas.append(delta)
+        
+        for i in range(0,len(timeDeltas)):
+            pathString += str(timeDeltas[i]) + " "
+            pathString += self.path[i+1] + " "
+            
+        pathString = pathString.strip()
+        pathString += "\n"
+        return pathString
+        
+    #hitting max line width in .txt files made name abbrev. necessary
+    def asDataFrameEntry(self):
+        pathString = self.visitorId + ";"
+        pathString += str(self.times[0]) + ";"
+        pathString += mapAbbrev[self.path[0]] +";"        
+        
+        #compute time deltas
+        timeDeltas = []
+        for i in range(1,len(self.times)):
+            delta = str(self.times[i] - self.times[i-1])
+            timeDeltas.append(delta)
+        
+        for i in range(0,len(timeDeltas)-1):
+            pathString += str(timeDeltas[i]) + ";"
+            pathString += mapAbbrev[self.path[i+1]] + ";"
+                
+        #print(self.visitorId)
+        #print(self.path)
+        #print(self.times)
+        pathString += str(timeDeltas[len(timeDeltas)-1]) +";"
+        pathString += mapAbbrev[self.path[len(timeDeltas)]]
+        
+        pathString += "\n"
+        return pathString
+            
         
 def isEntrance(gate):
     t = re.search("entrance[0-4]",gate)
@@ -50,47 +143,55 @@ with open('sensor.csv') as csvfile:
     for row in reader:
         lines.append(row)
 
-place = 0
 
-for line in lines:
-    place = place + 1
-    timestamp = line[0]
-    carId = line[1]
-    carType = line[2]
-    gate = line[3]
-    if (isEntrance(gate)):
-        if carId not in activeCars:
-            records[carId] = [VisitRecord(timestamp,gate,carId,False)]
-            activeCars[carId] = True    #car is entering
+place = 0
+for line in lines[1:]:
+        place = place + 1
+        timestamp = dt.strptime(line[0],"%Y-%m-%d %H:%M:%S")
+        carId = line[1]
+        carType = line[2]
+        gate = line[3]
+        
+        is_entrance = isEntrance(gate)
+        if carId not in records:
+            records[carId] = [VisitRecord(timestamp,gate,carId,is_entrance)]
+            activeCars[carId] = True
+            numVisits[carId] = 0
         else:
-            if carId not in records:        #test code. replace w error code later
-                print("A car has left without entering!")
-                print(line)
-            if carId in records:
-                if carId not in numVisits:
-                    records[carId][0].addObservation(timestamp,gate)
-                    numVisits[carId] = 0
-                else:                   
-                    if (activeCars[carId] == False):
-                        records[carId].append(VisitRecord(timestamp,gate,carId,False))
-                    else:
-                        records[carId][numVisits[carId]].addObservation(timestamp,gate)
-                    numVisits[carId] = numVisits[carId] + 1
-            activeCars[carId] = False   #car is leaving
-    else:
-        if carId not in activeCars:
-            activeCars[carId] = True    #car is entering
-            records[carId] = [VisitRecord(timestamp,gate,carId,True)]
-        else:
-            if carId not in numVisits: #if this is the first visit
-                records[carId][0].addObservation(timestamp,gate)
-            else:
+            if (is_entrance and activeCars[carId]):
+                #car is leaving
+                records[carId][numVisits[carId]].addObservation(timestamp,gate)
+                numVisits[carId] +=1
+            if (is_entrance and not activeCars[carId]):
+                records[carId].append(VisitRecord(timestamp,gate,carId,False))
+            if (is_entrance): 
+                activeCars[carId] = not activeCars[carId] 
+            
+            if (not is_entrance):
                 records[carId][numVisits[carId]].addObservation(timestamp,gate)
                 
+                    
                 
+#what is the longest path? 70
+
 #print results!
-visits = open("visits.txt",'w')
-for key in records:
-    for v in records[key]:
-        visits.write(v.toString())
-visits.close()
+if (writeVisits):
+    visits = open("visits.txt",'w')
+    for key in records:
+        for v in records[key]:
+            visits.write(v.toString())
+    visits.close()
+
+if (writePaths):
+    paths = open("paths.txt",'w')
+    for key in records:
+        for p in records[key]:
+            paths.write(p.pathString())
+    paths.close()
+    
+if (writedfEntries):
+    asDF = open("abbrev-paths.txt",'w')
+    for key in records:
+        for d in records[key]:
+            asDF.write(d.asDataFrameEntry())
+    asDF.close()
