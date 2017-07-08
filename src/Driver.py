ba74329eb27f258c8ca1303dd9b50505b637938d
hdickie <hdickie@dons.usfca.edu>
2017-07-07 17:03:25 -0700

    Driver for using plotPath

--- src/Driver.py
+++ src/Driver.py
@@ -4,32 +4,10 @@ Created on Fri Jun 02 17:58:25 2017
 
 @author: Hume Dickie
 """
+from IO import getIndividualRecords, plotPath
 
-#Driver.py
+records = getIndividualRecords()
 
-from IO import writeIndividualRecords, writeCampsiteRecords
-from IO import getIndividualRecords, getCampsiteRecords, getGroupHangouts
+plot = plotPath(records["20155201075209-669"][0])
 
-#records = getIndividualRecords()
-#writeIndividualRecords(records)
-
-campRecs = getCampsiteRecords()
-writeCampsiteRecords(campRecs)
-
-#sortedRecs = getGroupHangouts()
-  
-#weird car "20155705025759-63"
-# a ranger "20153722123707-242"
-#plot = plotPath(records["20155201075209-669"][0])
-
-#plot.show()
-#if this code isn't working then make sure config.py is right
-
-#campRecs = pathsToCampRecords("abbrev-paths.txt")
-
-#os.chdir(outputDirectory)
-#recs = open("campRecords.txt",'w')
-#recs.write("car.id;beginTimestamp;gate.name;duration\n")
-#for rec in campRecs:
-#    recs.write(rec)
-#recs.close()
+plot.show()
